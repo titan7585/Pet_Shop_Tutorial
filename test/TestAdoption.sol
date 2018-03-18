17c9c5a712b35f71ab1e936f0d0234e34fcc4293
Sourav Das <sourav.fb.90@gmail.com>
2018-03-18 11:37:55 +0530

    Initial commit

---
+++ test/TestAdoption.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.17;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Adoption.sol";
+
+contract TestAdoption {
+    Adoption adoption = Adoption(DeployedAddresses.Adoption());
+}
